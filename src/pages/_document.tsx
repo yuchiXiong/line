119baf46517d829fa58c172acf9fca68deb4833e
yuchiXiong <yuchi.xiong@foxmail.com>
2023-03-12 20:45:18 +0800

    feat: 实现基本的登录和注册页面结构

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className='h-screen'>
       <Head />
-      <body>
+      <body className='h-screen'>
         <Main />
         <NextScript />
       </body>
